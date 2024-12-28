c0c8e5a67a31ad81eceaa6a92aed6744bb7e19a6
mehedi-hasannnn <mhasan2999@gmail.com>
2024-12-29 02:06:31 +0600

    testimonial section loaded with review fake json data with star ratings

--- src/pages/Home/Testimonial/Testimonial.jsx
+++ src/pages/Home/Testimonial/Testimonial.jsx
@@ -7,6 +7,9 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
 
+import { Rating } from '@smastrom/react-rating';
+import '@smastrom/react-rating/style.css';
+
 const Testimonial = () => {
     const [reviews, setReviews] = useState([]);
     useEffect(()=>{
@@ -22,8 +25,13 @@ const Testimonial = () => {
         
         {
             reviews.map(review => <SwiperSlide key={review._id}>
-                <div className='m-224'>
-                    <p>{review.details}</p>
+                <div className='flex flex-col items-center mx-24 my-16'>
+                <Rating
+                style={{ maxWidth: 180 }}
+                value={review.rating}
+                readOnly
+                />
+                    <p className='py-8'>{review.details}</p>
                     <h3 className="text-2xl text-orange-400">{review.name}</h3>
                 </div>
             </SwiperSlide>)
