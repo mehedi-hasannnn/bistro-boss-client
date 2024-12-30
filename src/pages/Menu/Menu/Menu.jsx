e85ebda194b030306ccc2a8f3c8a3097d6dac02a
mehedi-hasannnn <mhasan2999@gmail.com>
2024-12-30 17:28:37 +0600

    Our menu page added with parallax banner

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg'
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 
 const Menu = () => {
     return (
@@ -7,7 +10,20 @@ const Menu = () => {
             <Helmet>
                 <title>Bistro | Menu</title>
             </Helmet>
-            <h2>Menu</h2>
+            <Cover img={menuImg} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+
+            <Cover img={menuImg} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+            
+            <Cover img={menuImg} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+            
+            <Cover img={menuImg} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+            
+            <Cover img={menuImg} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
