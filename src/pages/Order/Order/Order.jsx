f3dedb554fef916d2c45c2d4ba1b8829b36a4f46
mehedi-hasannnn <mhasan2999@gmail.com>
2024-12-30 20:11:17 +0600

    order food page added with react tab library

---
+++ src/pages/Order/Order/Order.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import orderCoverImg from '../../../assets/shop/order.jpg';
+import Cover from '../../Shared/Cover/Cover';
+import 'react-tabs/style/react-tabs.css';
+import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
+import useMenu from '../../../Hooks/useMenu';
+
+const Order = () => {
+    const [tabIndex, setTabIndex] = useState(0);
+    const [menu] = useMenu();
+    const desserts = menu.filter(item => item.category === 'dessert')
+    const soup = menu.filter(item => item.category === 'soup')
+    const salad = menu.filter(item => item.category === 'salad')
+    const pizza = menu.filter(item => item.category === 'pizza')
+    const offered = menu.filter(item => item.category === 'offered')
+    
+    return (
+        <div>
+            <Cover img={orderCoverImg} title="Order Food"></Cover>
+            <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+                <TabList>
+                    <Tab>Salad</Tab>
+                    <Tab>Pizza</Tab>
+                    <Tab>Soup</Tab>
+                    <Tab>Dessert</Tab>
+                    <Tab>Drinks</Tab>
+                </TabList>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+            </Tabs>
+        </div>
+    );
+};
+
+export default Order;
--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -6,8 +6,9 @@ const NavBar = () => {
     
     <li> <Link to="/">Home</Link> </li>
     <li> <Link to="/menu">Our Menu</Link> </li>
+    <li> <Link to="/order">Order Food</Link> </li>
 
-    <li><a>Item 3</a></li>
+    
 
     </>
     return (
