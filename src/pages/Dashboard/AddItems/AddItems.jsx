f5d429176789a13bb0d9cc32aefeeddbe79abace
mehedi-hasannnn <mhasan2999@gmail.com>
2025-01-08 21:51:47 +0600

    manage all items section data loaded

--- src/pages/Dashboard/AddItems/AddItems.jsx
+++ src/pages/Dashboard/AddItems/AddItems.jsx
@@ -3,12 +3,15 @@ import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 import { useForm } from 'react-hook-form';
 import { FaUtensils } from 'react-icons/fa';
 import useAxiosPublic from '../../../Hooks/useAxiosPublic';
+import useAxiosSecure from '../../../Hooks/useAxiosSecure';
+import Swal from 'sweetalert2';
 
 const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
 const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`
 const AddItems = () => {
-const { register, handleSubmit } = useForm();
+const { register, handleSubmit, reset } = useForm();
 const axiosPublic = useAxiosPublic();
+const axiosSecure = useAxiosSecure();
 const onSubmit = async (data) => {
     console.log(data)
     // Image upload to imgbb and then get an url
@@ -18,7 +21,31 @@ const onSubmit = async (data) => {
             'content-type' : 'multipart/form-data'
         }
     });
-    console.log(res.data);
+    if(res.data.success){
+        // now send the menu item data to the server with the image data
+        const menuItem = {
+            name: data.name,
+            category: data.category,
+            price: data.price,
+            recipe: data.recipe,
+            image: data.display_url
+        }
+        // Now 
+        const menuRes = await axiosSecure.post('/menu', menuItem)
+        console.log(menuRes.data)
+        if(menuRes.data.insertedId){
+            reset();
+            // show success pop up
+            Swal.fire({
+                position: "center",
+                icon: "success",
+                title: `${data.name} is added to the menu`,
+                showConfirmButton: false,
+                timer: 1500
+              });
+        }
+    }
+    console.log("with image url", res.data);
 
 };
     return (
