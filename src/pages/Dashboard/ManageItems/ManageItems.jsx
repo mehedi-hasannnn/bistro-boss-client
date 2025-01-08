1e0fb0bf76820dd4331838f99b08a62ce3830993
mehedi-hasannnn <mhasan2999@gmail.com>
2025-01-08 23:41:40 +0600

    update item functionality added

--- src/pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/pages/Dashboard/ManageItems/ManageItems.jsx
@@ -4,6 +4,7 @@ import useMenu from '../../../Hooks/useMenu';
 import { FaEdit, FaTrashAlt } from 'react-icons/fa';
 import Swal from 'sweetalert2';
 import useAxiosSecure from '../../../Hooks/useAxiosSecure';
+import { Link } from 'react-router-dom';
 
 const ManageItems = () => {
     const [menu, ,refetch] = useMenu();
@@ -82,9 +83,11 @@ const ManageItems = () => {
             </td>
             <td> ${item.price}</td>
             <td>
+              <Link to={`/dashboard/updateItem/${item._id}`}>
               <button className='btn btn-ghost'>
                 <FaEdit className='text-yellow-700 text-2xl'></FaEdit>
               </button>
+              </Link>
             </td>
             <td>
                  <button onClick={()=> handleDeleteItem(item)} className="btn btn-ghost btn-lg">
---
+++ src/pages/Dashboard/UpdateItem/UpdateItem.jsx
@@ -0,0 +1,128 @@
+import React from 'react';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import { useLoaderData } from 'react-router-dom';
+import { FaUtensils } from 'react-icons/fa';
+import { useForm } from 'react-hook-form';
+import Swal from 'sweetalert2';
+import useAxiosSecure from '../../../Hooks/useAxiosSecure';
+import useAxiosPublic from '../../../Hooks/useAxiosPublic';
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`
+
+const UpdateItem = () => {
+    const {name, category, recipe, price, _id} = useLoaderData();
+    const { register, handleSubmit, reset } = useForm();
+    const axiosPublic = useAxiosPublic();
+    const axiosSecure = useAxiosSecure();
+const onSubmit = async (data) => {
+    console.log(data)
+    // Image upload to imgbb and then get an url
+    const imageFile = {image: data.image[0] }
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+        headers: {
+            'content-type' : 'multipart/form-data'
+        }
+    });
+    if(res.data.success){
+        // now send the menu item data to the server with the image data
+        const menuItem = {
+            name: data.name,
+            category: data.category,
+            price: data.price,
+            recipe: data.recipe,
+            image: res.data.data.display_url,
+        }
+        // Now 
+        const menuRes = await axiosSecure.patch(`/menu/${_id}`, menuItem)
+        console.log(menuRes.data)
+        if(menuRes.data.modifiedCount > 0){
+            // show success pop up
+            Swal.fire({
+                position: "center",
+                icon: "success",
+                title: `${data.name} is updated to the menu`,
+                showConfirmButton: false,
+                timer: 1500
+              });
+        }
+    }
+    console.log("with image url", res.data);
+
+};
+
+    return (
+        <div>
+            <SectionTitle heading="Update an Item" subHeading="Refresh Info"></SectionTitle>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+
+      <label className="form-control w-full">
+  <div className="label">
+    <span className="label-text">Recipe Name*</span>
+  </div>
+  <input type="text" defaultValue={name} placeholder="Recipe Name" {...register('name', {required: true})} required className="input input-bordered w-full my-4" />
+  
+</label>
+
+
+    <div className='flex gap-6'>
+        {/* Category */}
+        <label className="form-control w-full">
+  <div className="label">
+    <span className="label-text">Category*</span>
+  </div>
+  <select defaultValue={category} {...register('category', {required: true})}
+      className="select select-bordered w-full ">
+        <option disabled value="default">Select a category</option>
+        <option value="salad">Salad</option>
+        <option value="pizza">Pizza</option>
+        <option value="soup">Soup</option>
+        <option value="dessert">Dessert</option>
+        <option value="drinks">Drinks</option>
+      </select>
+  
+</label>
+
+        {/* Price */}
+        <label className="form-control w-full">
+  <div className="label">
+    <span className="label-text">Price*</span>
+  </div>
+  <input 
+  type="number" 
+  defaultValue={price}
+  step="0.01" 
+  placeholder="Price" 
+  {...register('price', { 
+    required: true, 
+    setValueAs: (value) => parseFloat(value) 
+  })} 
+  className="input input-bordered w-full" 
+/>
+
+  
+</label>
+
+</div>
+{/* Recipe Details */}
+<label className="form-control mt-2">
+  <div className="label">
+    <span className="label-text">Recipe Details*</span>
+  </div>
+  <textarea {...register('recipe', {required: true})} className="textarea textarea-bordered h-24" defaultValue={recipe} placeholder="Recipe Details"></textarea>
+</label>
+
+<div className='form-control w-full my-6'>
+<input type="file" {...register('image', {required: true})} className="file-input file-input-bordered" />
+</div>
+
+<button className='btn bg-gradient-to-r from-yellow-500 to-yellow-300'>Update Item <FaUtensils className='ml-4'></FaUtensils> </button>
+      
+        </form>
+        
+        </div>
+    );
+};
+
+export default UpdateItem;
